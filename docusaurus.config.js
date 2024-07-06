1fe433dc0d54e5838dfab8a20e865c28aa92962e
Emilia <47392011+emilianya@users.noreply.github.com>
2024-07-06 13:38:52 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'LIT Devs',
-  url: 'https://docs.litdevs.org',
+  url: 'https://litdevs.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
